09dd32fa85119de773c747f32eb9dfee70f9b453
AraceliDanielaRamirezLopez <daniiela.ari33@gmail.com>
2020-10-22 16:20:28 -0500

    archivo dias seg
    archivo realizado en clase

--- ejercicios/dias.seg.c
+++ ejercicios/dias.seg.c
@@ -1,11 +1,16 @@
-#include <studio.h>
+#include <stdio.h>
 
-int main(){
-	int dias:
+int main()
+{
+	//Declaracion de variables 
+	int dias;
 	float resultado;
-	
+	//instrucciones para el usuario 
+	printf( "Ingrese un numero de dias para calcular los segundos que tiene ");
+	//Leemos la variable días 
+	scanf ("%d", &dias);
 	resultado = dias *(24*60*60);
-	printf ("En %d dias hay %f segundos", dias, resultado  )
+	printf ("\tEn %d dias hay %f segundos.\n", dias, resultado );
 	return 0; 
 
 }
