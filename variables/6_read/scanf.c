2145630ef14a29287554098741c9640ffe4180a8
AlvaroJGL-GAR <71991820+AlvaroJGL-GAR@users.noreply.github.com>
2020-10-26 02:27:27 -0600

    Arreglo de scanf
    Se trunco a 2 decimales los resultados

--- variables/6_read/scanf.c
+++ variables/6_read/scanf.c
@@ -8,6 +8,6 @@ printf("Introduce radio (entero): ");
 scanf("%d", &r);
 l=(PI*2)*r; 
 a=PI*(pow(r,2));
-printf("La longitud de la circunferencia vale %0.3f\n", l);
-printf("El área del círculo vale %0.3f\n", a);
+printf("La longitud de la circunferencia vale %0.2f\n", l);
+printf("El área del círculo vale %0.2f\n", a);
 }
