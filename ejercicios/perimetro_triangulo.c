327d6d3db6da03275a8c952592218b8c2b2fe3d2
AngelicaBaca <72226635+AngelicaBaca666@users.noreply.github.com>
2020-10-26 12:30:29 -0600

    Se realizó calculo de perimetro

---
+++ ejercicios/perimetro_triangulo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include<stdlib.h>
+
+int main()
+
+{
+	float a, b, c, Perimetro; //Declarando variables
+	
+	printf ("Dame el Lado a: ") ;
+	scanf (" %f", &a) ;
+	
+	printf ("Dame el Lado b: ") ;
+	scanf (" %f" , &b) ;
+	
+	printf ("Dame el Lado c: ") ;
+	scanf ("%f", &c) ;
+	
+	Perimetro = a+b+c ; //Realiza calculo de perimetro
+	
+	printf ("Perimetro : %.2f\n" , Perimetro );
+	
+	
+	
+	
+	return 0;
+	
+}
+
+
+
+
