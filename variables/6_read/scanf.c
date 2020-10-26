3df31ac16859dda8e3afcd8c137254df1cdd84c4
AlvaroJGL-GAR <71991820+AlvaroJGL-GAR@users.noreply.github.com>
2020-10-26 02:22:32 -0600

    Mejora de scanf
    Se agrego una potencia y se trunco a 3 decimales los resultados

--- variables/6_read/scanf.c
+++ variables/6_read/scanf.c
@@ -6,8 +6,8 @@ int r;
 float l, a;
 printf("Introduce radio (entero): ");
 scanf("%d", &r);
-l=2*PI*r; 
-a=PI*r*r;
-printf("La longitud de la circunferencia vale %f\n", l);
-printf("El área del círculo vale %f\n", a);
+l=(PI*2)*r; 
+a=PI*(pow(r,2));
+printf("La longitud de la circunferencia vale %0.3f\n", l);
+printf("El área del círculo vale %0.3f\n", a);
 }
