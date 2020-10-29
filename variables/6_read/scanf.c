cc3b8b2b5d286e93058368baa0e67f869cf2ef6e
Dannas Cornell <dannas.cornell@gmail.com>
2020-10-29 13:11:15 -0600

    Update scanf.c
    Se agregó la librería math.h y la función de potencia a la operación de área.

--- variables/6_read/scanf.c
+++ variables/6_read/scanf.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 #define PI 3.14159
+
 int main()
 {
 	
 	int  r;
-	float l, a, c, d;
+	float l, a;
 	printf("Introduce radio(entero): ");
 	scanf("%d",&r);
-	printf("\nPotencia\n");
-	c= pow (PI,d);
-	printf("\t%0.2f elevado a la potencia / %0.2f es  %0.2f\n",PI,l,a,c,d);
-	printf("Multiplicacion\n");
-	l=2*PI*r;
-	printf("\t%0.2f * %0.2f * %0.2f = %0.2f\n",2,PI,r);
-	printf("Multiplicacion\n");
-	a=PI*r*r;
-	printf("\t%0.2f * %0.2f * %0.2f = %0.2f\n",PI,r,r);
+	
+	l=(2 * PI) * r;
+	a=pow(r,2)*PI;
 	printf("La longitud de la circunferencia vale %0.2f/n",l);
 	printf("El area del circulo vale %0.2f/n",a);		
 }
