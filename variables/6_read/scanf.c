0413355edd14b4c6d236396f8fba783a16b4faa0
Dannas Cornell <dannas.cornell@gmail.com>
2020-10-29 12:41:47 -0600

    Update scanf.c
    Se agregó la función de potencia

--- variables/6_read/scanf.c
+++ variables/6_read/scanf.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <math.h>
 #define PI 3.14159
+
 int main()
 {
 int r;
@@ -7,7 +9,7 @@ float l, a;
 printf("Introduce radio (entero): ");
 scanf("%d", &r);
 l= (2*PI*r); 
-a= (PI*r*r);
+a = PI * pow(r,2);
 printf("La longitud de la circunferencia vale %0.2f\n", l);
 printf("El área del círculo vale %0.2f\n", a);
 }
