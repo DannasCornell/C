90b0f1bc38aadeacbb7da16b483bf6333aaad7a4
Dannas Cornell <dannas.cornell@gmail.com>
2020-10-29 13:14:20 -0600

    Update scanf.c
    Se agregó función de potencia para cálculo de área.

--- variables/6_read/scanf.c
+++ variables/6_read/scanf.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 #define PI 3.14159
+
 int main()
 {
 int r;
 
-float l, a, p;
+float l, a;
 printf("Introduce radio (entero): ");
 scanf("%d", &r);
 
 l=2*PI*r; 
-a=PI*r*r;
+a=pow(r,2)*PI;
 
 printf("La longitud de la circunferencia vale %0.2f\n", l);
 printf("El Area del circulo vale %0.2f\n", a);
-
-p= pow(PI, r);
-printf("\t%0.2f elevado a la potencia %0.2i vale %0.2f\n", PI, r, p);
-p= pow(r, PI);
-printf("\t%0.2i elevado a la potencia %0.2f vale %0.2f\n", r, PI, p); 
+  
 return 0;
 }
 
